7d1622a7b531617fb18dd43afa2821f6a928771e
rafactor <avila.rafael@gmail.com>
2019-01-14 17:23:30 -0500

    switching computers - nothing solved

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,115 @@
-var responses = [
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      },
-]
+var fs = require('fs');
+var dataArray = [];
+var responseObject = {};
 
-module.exports = responses
+fs.readFile('./friendsdata.txt','utf8',function(err, data){
+    // if (err) throw err;
+
+     responseObject = JSON.parse(data);
+    // dataArray.push(responseObject)
+    console.log('from file')
+    console.log(responseObject);
+    next()
+})
+
+// var response = [
+//     {
+//         "name":"Ahmed",
+//         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//         "scores":[
+//             5,
+//             1,
+//             4,
+//             4,
+//             5,
+//             1,
+//             2,
+//             5,
+//             4,
+//             1
+//           ]
+//       },
+// ]
+
+
+
+var input = {
+    "name":"ssss",
+            "photo":"https://test.jpg",
+            "scores":[
+                5,
+                1,
+                4,
+                1,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+              ]
+          
+}
+
+function postFriend(){
+
+    console.log('input')
+    responseObject.push(input)
+    console.log('postiing')
+    console.log(responseObject)
+    
+    fs.writeFile('./friendsdata.txt',JSON.stringify(responseObject),function(err){
+	if (err) {
+        return console.log(err);
+        }
+    
+    console.log("updated");
+});
+}
+
+function next(){
+    console.log('response')
+
+    console.log(responseObject)
+    
+    console.log('dataArray')
+    console.log(dataArray);
+    console.log('input')
+    
+    console.log(input)
+    console.log('dataArray')
+    
+    dataArray.push(input)
+    console.log(dataArray);
+    
+}
+
+// postFriend()
+
+function compareProfiles(){
+    
+    // read the friends data file
+    
+
+    // set the last record as the current user
+
+    // set numberOfUsers as the lenght of the users array
+
+    //loop through all users in the
+
+
+}
+
+// * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+// * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+//   * Example:
+//     * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+//     * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+//     * Total Difference: **2 + 1 + 2 =** **_5_**
+// * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+// * The closest match will be the user with the least amount of difference.
+
+// 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+// * The modal should display both the name and picture of the closest match.
+
+// module.exports = response
